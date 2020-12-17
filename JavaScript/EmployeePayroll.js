e29436d2fc6a07c0c690cd5c690a7f4791629596
bharambeakshay <bharambeakshay1997@gmail.com>
2020-12-18 00:46:17 +0530

    [Akshay]  Refactor .  validations for name date are added UC10 UC11 UC12

--- JavaScript/EmployeePayroll.js
+++ JavaScript/EmployeePayroll.js
@@ -1,17 +1,13 @@
-/**
- *  @class :  In EmployeePayRoll class we are assigning  and retrieving values of respective files
- */
 class EmployeePayRoll {
-    //we are adding this id to use it in the form by which we will update delete and find values
+    // getter and setter method
     get id() { return this._id; }
-    set(id) {
+    set id(id) {
         this._id = id;
     }
 
     get name() { return this._name; }
     set name(name) {
-        //validating only name with min 3 letters and first letter caps is allowed  
-        let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z]{2,}$');
+        let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
         if (nameRegex.test(name))
             this._name = name;
         else throw 'Name is Incorrect!';
@@ -20,23 +16,6 @@ class EmployeePayRoll {
     get profilePic() { return this._profilePic; }
     set profilePic(profilePic) {
         this._profilePic = profilePic;
-    }
-
-    /**
-     * @function : toString() prints the respective data
-     * @param { }: No parameters are needed 
-     */
-
-    toString() {
-        //giving the type of date required
-        const options = { year: 'numeric', month: 'short', day: 'numeric' };
-        //if start date is not in the given datatype it will pass the value undefined to empdate
-        const empDate = !this.startDate ? "undefined" :
-            this.startDate;
-        //here in return for start date we are passing empDate as value                
-        return "id=" + this.id + ", name='" + this.name + ", gender='" + this.gender +
-            ", profilePic='" + this.profilePic + ", department=" + this.department +
-            ", salary=" + this.salary + ", startDate=" + empDate + ", note=" + this.note;
     }
 
     get gender() { return this._gender; }
@@ -59,18 +38,21 @@ class EmployeePayRoll {
         this._note = note;
     }
 
-    // date func will provide todays date
     get startDate() { return this._startDate; }
     set startDate(startDate) {
         let now = new Date();
-        //if it is a future date it will throw error
         if (startDate > now) throw 'Start Date is a Future Date!';
-        //abs return absolute value of a function
         let diff = Math.abs(now.getTime() - startDate.getTime());
-        console.log(diff);
-        //converting into proper day format of max 30 days
         if (diff / (1000 * 60 * 60 * 24) > 30)
             throw 'Start Date is beyond 30 Days!';
         this._startDate = startDate;
+    }
+
+    toString() {
+        const options = { year: 'numeric', month: 'short', day: 'numeric' };
+        const empDate = !this.startDate ? "undefined" : this.startDate;
+        return "id=" + this.id + ", name='" + this.name + ", gender='" + this.gender +
+            ", profilePic='" + this.profilePic + ", department=" + this.department +
+            ", salary=" + this.salary + ", startDate=" + empDate + ", note=" + this.note;
     }
 }
--- JavaScript/employeePayrollApp.js
+++ JavaScript/employeePayrollApp.js
@@ -1,4 +1,4 @@
-//UC8
+// UC8
 // salary bar 
 const salary = document.querySelector('#salary');
 const output = document.querySelector('.salary-output');
@@ -7,3 +7,45 @@ output.textContent = salary.value;
 salary.addEventListener('input', function () {
     output.textContent = salary.value;
 });
+
+//UC10 UC11 UC12 validations for Name and Date
+
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    // validating error for name
+    const name = document.querySelector('#name');
+    const textError = document.querySelector('.text-error')
+    name.addEventListener('input', function () {
+        if (name.value.length === 0) {
+            textError.textContent = "";
+            return;
+        }
+        try {
+            (new EmployeePayRoll()).name = name.value;
+            textError.textContent = "";
+        } catch (e) {
+            textError.textContent = e;
+        }
+    });
+
+    //validaitng errors for date
+    const date = document.querySelector('#date');
+    const dateError = document.querySelector('.date-error');
+    const getInputValueById = (id) => {
+        let value = document.querySelector(id).value;
+        return value;
+    }
+    date.addEventListener('click', function () {
+        let startDate = getInputValueById('#day') + " " + getInputValueById('#month') + " " +
+            getInputValueById('#year');
+        try {
+            (new EmployeePayRoll()).startDate = new Date(Date.parse(startDate));
+            dateError.textContent = "";
+        } catch (e) {
+            dateError.textContent = e;
+        }
+
+    });
+})
+
+
