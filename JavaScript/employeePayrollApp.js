d7b4d1228a3de644721b04f3a02c7eaa23343c7d
bharambeakshay <bharambeakshay1997@gmail.com>
2020-12-18 01:44:19 +0530

    [Akshay]  Refactor .  object is created using alert

--- JavaScript/employeePayrollApp.js
+++ JavaScript/employeePayrollApp.js
@@ -1,51 +1,77 @@
-// UC8
-// salary bar 
-const salary = document.querySelector('#salary');
-const output = document.querySelector('.salary-output');
-//in range button the output shown should always be equal to value the user is updating
-output.textContent = salary.value;
-salary.addEventListener('input', function () {
-    output.textContent = salary.value;
-});
 
-//UC10 UC11 UC12 validations for Name and Date
-
+//UC10 validations for Name and Date
+// UC11 Ability to create Emp payrollObject
 
 window.addEventListener('DOMContentLoaded', (event) => {
-    // validating error for name
-    const name = document.querySelector('#name');
+    const name = document.querySelector("#name")
     const textError = document.querySelector('.text-error')
     name.addEventListener('input', function () {
-        if (name.value.length === 0) {
-            textError.textContent = "";
-            return;
+        if (name.value.length == 0) {
+            textError.textContent = ""
+            return
         }
         try {
-            (new EmployeePayRoll()).name = name.value;
-            textError.textContent = "";
-        } catch (e) {
-            textError.textContent = e;
-        }
+            (new EmployeePayRoll).name = name.value
+            textError.textContent = ""
+        } catch (e) { textError.textContent = e }
     });
-
-    //validaitng errors for date
-    const date = document.querySelector('#date');
-    const dateError = document.querySelector('.date-error');
-    const getInputValueById = (id) => {
-        let value = document.querySelector(id).value;
-        return value;
-    }
-    date.addEventListener('click', function () {
-        let startDate = getInputValueById('#day') + " " + getInputValueById('#month') + " " +
-            getInputValueById('#year');
-        try {
-            (new EmployeePayRoll()).startDate = new Date(Date.parse(startDate));
-            dateError.textContent = "";
-        } catch (e) {
-            dateError.textContent = e;
-        }
-
+    //UC8
+    const salary = document.querySelector('#salary');
+    const output = document.querySelector('.salary-output');
+    output.textContent = salary.value;
+    salary.addEventListener('input', function () {
+        output.textContent = salary.value
     });
 })
 
+const save = () => {
+    try {
+        let employeeObject = createEmployeePayroll();
 
+    } catch (e) {
+        return;
+    }
+}
+
+const createEmployeePayroll = () => {
+    let employeePayrollData = new EmployeePayRoll();
+    try {
+        employeePayrollData.name = getInputValueById('#name');
+    }
+    catch (e) {
+        setTextValue('.text-error', e);
+        throw e;
+    }
+
+    employeePayrollData.id = getSelectedValues('[name =id]')
+    employeePayrollData.profilePic = getSelectedValues('[name=profile]').pop()
+    employeePayrollData.gender = getSelectedValues('[name=gender]').pop()
+    employeePayrollData.department = getSelectedValues('[name=department]')
+    employeePayrollData.salary = getInputValueById('#salary')
+    employeePayrollData.note = getInputValueById('#notes')
+    let date = getInputValueById('#day') + " " + getInputValueById('#month') + " " +
+        getInputValueById('#year')
+    employeePayrollData.date = Date.parse(date)
+    alert(employeePayrollData.toString())
+    return employeePayrollData
+}
+
+const getSelectedValues = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    let setItems = [];
+    allItems.forEach(item => {
+        if (item.checked)
+            setItems.push(item.value);
+    });
+    return setItems;
+}
+
+const getInputElementValue = (id) => {
+    let value = document.querySelector(id).value;
+    return value;
+}
+
+const getInputValueById = (id) => {
+    let value = document.querySelector(id).value;
+    return value;
+}
