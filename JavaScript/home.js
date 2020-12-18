ab3a2dba54c271c3ed6e605c52648a6278ec7b64
bharambeakshay <bharambeakshay1997@gmail.com>
2020-12-19 01:31:55 +0530

    [Akshay] Refactor . dynamic content  loaded  from JSON object

--- JavaScript/home.js
+++ JavaScript/home.js
@@ -1,44 +1,64 @@
+const createEmployeePayrollJSON = () => {
+    let empPayrollListLocal = [
+        {
+            _name: 'Chandler Bing',
+            _gender: 'male',
+            _department: [
+                'Engineering', 'Hr'
+            ],
+            _salary: '300000',
+            _startDate: '8 June 2020',
+            _note: '',
+            _id: new Date().getTime(),
+            _profilePic: '../Assets/profile-images/Ellipse -3.png'
+        },
+        {
+            _profilePic: '../Assets/profile-images/Ellipse -1.png',
+            _name: 'Monica Geller',
+            _gender: 'female',
+            _department: [
+                'Engineering', 'Sale'
+            ],
+            _salary: '100000',
+            _startDate: '15 April 2020',
+            _note: '',
+            _id: new Date().getTime(),
+
+        }
+    ]
+    return empPayrollListLocal
+}
+
 window.addEventListener('DOMContentLoaded', (event) => {
     createInnerHtml();
 })
+
 function createInnerHtml() {
     const headerHtml = `<tr><th></th><th>Name</th><th>Gender</th><th>Department</th>
         <th>Salary</th><th>start Date</th><th>Actions</th></tr>`
-    const innerHtml = `${headerHtml}
-    <tr>
-    <td><img src="../Assets/profile-images/Ellipse -1.png" class="profile" width="30px" alt=""></td>
-        <td>Rachel Green</td>
-        <td>Female</td>
+    let innerHtml = `${headerHtml}`
+    let empPayrollList = createEmployeePayrollJSON()
+    for (const empPayrollData of empPayrollList) {
+        innerHtml = `${innerHtml}
+        <tr>
+        <td><img src="${empPayrollData._profilePic}" class="profile" width="30px" alt=""></td>
+        <td>${empPayrollData._name}</td>
+        <td>${empPayrollData._gender}</td>
+        <td>${getDeptHtml(empPayrollData._department)}</td>
+        <td>${empPayrollData._salary}</td>
+        <td>${empPayrollData._startDate}</td>
         <td>
-            <div class="dept-label">Engineer</div>
-            <div class="dept-label">HR</div>
-            <div class="dept-label">Others</div>
+            <img id="1" name="${empPayrollData._id}" onclick="remove(this)" alt="delete" width="30px" src="../Assets/icons/delete-black-18dp.svg">
+            <img id="1" name="${empPayrollData._id}" onclick="update(this)" alt="edit" width="30px" src="../Assets/icons/create-black-18dp.svg  ">
         </td>
-        <td>400000</td>
-        <td>15 March 2020</td>
-        <td>
-            <img id="1" onclick="remove(this)" alt="delete" width="30px"
-                src="../Assets/icons/delete-black-18dp.svg">
-                <img id="1" onclick="update(this)" alt="edit" width="30px"
-                    src="../Assets/icons/create-black-18dp.svg">
-</td>
-</tr>
-            <tr>
-                <td><img src="../Assets/profile-images/Ellipse -2.png" class="profile" width="30px" alt=""></td>
-                    <td>Joey Tribbiani</td>
-                    <td>Male</td>
-                    <td>
-                        <div class="dept-label">Engineer</div>
-                        <div class="dept-label">HR</div>
-                    </td>
-                    <td>400000</td>
-                    <td>15 May 2020</td>
-                    <td>
-                        <img id="1" onclick="remove(this)" alt="delete" width="30px"
-                            src="../Assets/icons/delete-black-18dp.svg">
-                            <img id="1" onclick="update(this)" alt="edit" width="30px"
-                                src="../Assets/icons/create-black-18dp.svg">
-</td>
-</tr>`
+    </tr>`
+    }
     document.querySelector('#display').innerHTML = innerHtml
+}
+function getDeptHtml(deptList) {
+    let deptHtml = ''
+    for (const dept of deptList) {
+        deptHtml = `${deptHtml} <div class='dept-label'>${dept}</div>`
+    }
+    return deptHtml
 }
