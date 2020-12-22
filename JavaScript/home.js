48a6b078a308db86c82d9788d5673bc5a12011d1
bharambeakshay <bharambeakshay1997@gmail.com>
2020-12-22 17:36:54 +0530

    [Akshay] Refactor . variable name is fixed

--- JavaScript/home.js
+++ JavaScript/home.js
@@ -54,14 +54,9 @@ const remove = (node) => {
     window.location.reload();
 }
 
-const update= (node)=>{
-
-    let empPayrollData= empPayrollList.find(empData=>empData._id= node.id);
-
-    if(!empPayrollData) return;
-
-    localStorage.setItem('editEmp',JSON.stringify(empPayrollData));
-  
+const update = (node) => {
+    let empPayrollData = empPayrollList.find(empData => empData._id = node.id);
+    if (!empPayrollData) return;
+    localStorage.setItem('editEmp', JSON.stringify(empPayrollData));
     window.location.replace(site_properties.add_emp_payroll_page);
-  }
-  
+}
--- JavaScript/site_properties.js
+++ JavaScript/site_properties.js
@@ -1,5 +1,5 @@
 //site properties are used so that we dont need to hard code all the links manually
 let site_properties = {
-    home_page: "../Pages/Home.html",
+    home_page: "../Pages/home.html",
     add_emp_payroll_page: "../Pages/employeePayrollApp.html"
 }
